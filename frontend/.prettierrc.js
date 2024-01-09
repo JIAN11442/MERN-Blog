76c6a2e1a8ffc00555fa42a7f630ce8e275aa7d1
JIAN11442 <yikjian11442@gmail.com>
2024-01-09 16:36:18 +0800

    DONE: Typescript + Vite initial environment setting

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 120,
+    tslintintegration: true,
+    trailingComma: "all",
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    endOfLine: "auto",
+  };
+  
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+      extend: {
+          fontFamily: {
+            inter: ["'Inter'", "sans-serif"],
+            gelasio: ["'Gelasio'", "serif"]
+          },
+          colors: {
+            'white': {
+              'custom':'#FFFFFF'
+            },
+            'black': {
+              'custom':'#242424'
+            },
+            'grey': {
+              'custom':'#F3F3F3',
+              'dark':'#6B6B6B'
+            },
+            'red': {
+              'custom':'#FF4E4E'
+            },
+            'purple': {
+              'custom':'#8B46FF'
+            },
+            'twitter': '#1DA1F2',
+        },
+  
+        fontSize: {
+            'sm': '12px',
+            'base': '14px',
+            'xl': '16px',
+            '2xl': '20px',
+            '3xl': '28px',
+            '4xl': '38px',
+            '5xl': '50px',
+        },
+      },
+
+  },
+  variants: {},
+  plugins: [],
+};
